0ad416852decca94d9c92b79b79581bcb272c75f
Inuka Ranmira <88465820+InukaRanmira@users.noreply.github.com>
2021-12-18 10:41:05 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -21,5 +21,5 @@ class Config(object):
     PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "0").split()))
     PRO_USERS.append(OWNER_ID)
     MONGODB_URI = os.environ.get("MONGODB_URI", "")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-100"))
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
